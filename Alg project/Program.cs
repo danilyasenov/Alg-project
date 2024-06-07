7e124762736f256e55ffe6387092f4d98e69b3e1
Данил Ясенов <yasenov_d@auca.kg>
2024-06-07 19:47:27 +0600

    Нуржиг создал класс Item с моего компьютера Я создал коснтруктор и поля Hashtable

--- Alg project/Program.cs
+++ Alg project/Program.cs
@@ -9,13 +9,31 @@ class Translator
 }
 public class Hashtable
 {
+    private const int _InitCapacity = 10;
+    private LinkedList<Item>[] buckets;
+    private int _count;
+    private int _capacity;
 
+    public int Count => _count;
+
+    public Hashtable()
+    {
+        _capacity = _InitCapacity;
+        buckets = new LinkedList<Item>[_capacity];
+        _count = 0;
+    }
 }
 
 public class Item
 {
+    public string Key { get; }
+    public string Value { get; }
 
-
+    public Item(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
 
 
 }
